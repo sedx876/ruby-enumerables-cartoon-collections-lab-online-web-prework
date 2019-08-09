616cfc80809168525b97811cb81c4c9b8c239c9e
Sharon Watkins <swatkins876@gmail.com>
2019-08-09 11:56:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+#def roll_call_dwarves# code an argument here
+  ["Grumpy", "Happy", "Sleepy", "Bashful", "Sneezy", "Dopey", "Doc"].each_with_index { |word, index| puts "index: #{index} for #{word}" }
+
 end
 
 def summon_captain_planet# code an argument here
