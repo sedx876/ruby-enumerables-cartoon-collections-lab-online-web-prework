35ff854388a71eeabebdbe25cd7b8798562f1cc8
Sharon Watkins <swatkins876@gmail.com>
2019-08-09 12:09:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@
   ["Grumpy", "Happy", "Sleepy", "Bashful", "Sneezy", "Dopey", "Doc"].each_with_index { |word, index| puts "#{index} #{word}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+#def summon_captain_planet# code an argument here
+ ["earth", "wind", "fire", "water", "heart"].collect { |word| puts word.capitalize + "!" }
 end
 
 def long_planeteer_calls# code an argument here
